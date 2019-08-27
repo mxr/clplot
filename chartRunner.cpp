95eb7e9b4cf4a6c4ba634079144b5ea57f0b9d88
k-vernooy <kai.a.vernooy@gmail.com>
2019-08-27 07:27:14 -0400

    Create chart object

--- chartRunner.cpp
+++ chartRunner.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "chart.hpp"
 
 using namespace std;
 
@@ -11,27 +12,27 @@ int main(int argc, char *argv[]) {
     int typeCount;
 
     string chartType;
+    string data;
 
     for (int i = 1; i < argc; i++) {
-        // if ( argv[i] == string("-d")) {
-        //     dataInc = true;
-        // }
-        // else {
-        //     if ( dataInc == true ) {
-        //         cout << "data:" << argv[i] << endl;
-
-        //         if ( dataCount == 2 ){
-        //             cout << "Too many data series!\n";
-        //             exit (EXIT_FAILURE);
-        //         }
-        //         dataCount++;
-        //     }
-        //     else {
-        //         cout << argv[i] << endl;
-        //     }
-        //     dataInc = false;
-        // }
-
+        if ( argv[i] == string("-d")) {
+            dataInc = true;
+        }
+        else {
+            if (dataInc) {
+                data = argv[i];
+                string data = argv[i];
+                if ( dataCount == 1 ){
+                    cout << "Multiple data series are not supported yet. Please use a single -d flag.\n";
+                    exit (EXIT_FAILURE);
+                }
+                dataCount++;
+            }
+            else {
+                cout << "";
+            }
+            dataInc = false;
+        }
 
         //-==================================================
         //   Records the data type in the chartType variable
@@ -56,7 +57,13 @@ int main(int argc, char *argv[]) {
             typeInc = false;
         }
     }
-    std::cout << "Chart type:" << chartType << endl;
+
+    cout << chartType << endl;
+    cout << data << endl;
+
+    Chart chart;
+    chart.addType(chartType);
+    chart.getType();
 }
 
 
