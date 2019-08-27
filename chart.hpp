ea2edd0558a4d33881e6847f859a8ecae04665f9
k-vernooy <kai.a.vernooy@gmail.com>
2019-08-27 08:56:46 -0400

    Added data functions

--- chart.hpp
+++ chart.hpp
@@ -1,13 +1,18 @@
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Chart {
-  
+  int datasize;
   string type;
+  vector<float> data;
 
 public:
     void addType(string type);
     string getType();
-  
+
+    void addData(string data);
+    void getData();
+
 };
