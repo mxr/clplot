1e3417441e7f150fba7dacca67148763f335aeb8
k-vernooy <kai.a.vernooy@gmail.com>
2019-09-08 19:03:04 -0400

    Begin window function for drawing basic window shape

--- src/functions.cpp
+++ src/functions.cpp
@@ -69,8 +69,40 @@ vector<float> Chart::getData() {
 
 void Chart::winSet(int height, int width, int posX, int posY, int termHeight, int termWidth) {
     // height will be lt termheight
-    if ( height + posY > termHeight || width + posX > termWidth) {
-        cout << "chart: \e[31merror:\e[0m The chart size and position are greater than the terminal window.";
-        return;
+
+    // cout << "chart height: " << height << endl;
+    // cout << "position y: " << posY << endl;
+
+    if ( height + posY > termHeight) {
+        cout << "chart: \e[31merror:\e[0m The sum of the chart height (\e[93m" << height << "\e[0m) and vertical position (\e[93m" << posY <<  "\e[0m) is greater than the terminal height (\e[93m" << termHeight << "\e[0m)." << endl;
+        exit(1);
+    }
+    else if ( width + posX > termWidth) {
+        cout << "chart: \e[31merror:\e[0m The sum of the chart width (\e[93m" << width << "\e[0m) and horizontal position (\e[93m" << posX <<  "\e[0m) is greater than the terminal width (\e[93m" << termWidth << "\e[0m)." << endl;
+        exit(1);
+    }
+    else {
+        cout << "Chart height: " << height << endl;
+        cout << "Chart width: " << width << endl;
+
+        // Chart::window[1][2] = "gi";
+
+        cout << window.size() << endl;
+
+        
+        for (int i = 0; i < 10; i++) {
+            vector<int> row; // Create an empty row
+            for (int j = 0; j < 20; j++) {
+                row.push_back(i * j); // Add an element (column) to the row
+            }
+            vec.push_back(row); // Add the row to the main vector
+            // create a vector row then push this vector to the vector of vectors.
+        }
+        // chart tl = posx, posy
+        // chart tr = posx + width, posy
+        // chart bl = posx, posy + height
+        // chart br = posx + width, posy + height
+ 
+
     }
 }
