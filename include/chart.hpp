686704408b05f28af30a371f66995be6e3f2dada
k-vernooy <kai.a.vernooy@gmail.com>
2019-10-05 11:38:03 -0400

    Refactored class system; added line  and bar derived classed

--- include/chart.hpp
+++ include/chart.hpp
@@ -4,13 +4,8 @@
 using namespace std;
     
 class Chart {
+
     public: 
-
-        vector<string> lineChars = { "╭", "╮", "╰", "╯", "│", "─" };
-        vector<string> lineCharsBold = { "╔", "╗", "╚", "╝", "║", "═" };
-        vector<string> lineCharsArea = { "▗", "▖", "▙", "▟" , "▐" , "▄" };
-        vector<string> sharpLineChars = { "┌", "┐", "└", "┘", "│", "─" };
-
         int width;
         int height;
         int posY;
@@ -20,28 +15,53 @@ class Chart {
 
         string color;
         string type;
-        vector<float> data;
 
+        vector<float> data;
         vector<vector<string> > window;
+        vector<string> winChars = { "╭", "╮", "╰", "╯", "│", "─" };
+        vector<string> winCharsSharp = { "┌", "┐", "└", "┘", "│", "─" };
 
     public:
-
+        
+        // metadata methods
+        void addColor(string newColor);
         void addType(string type);
         string getType();
-
-        void addColor(string newColor);
-
         void addData(string data, int lines, int cols);
         vector<float> getData();
 
+        // window methods
         void winSet(int height, int width, int posX, int posY, int termHeight, int termWidth);
+        void move();
+
+        // drawing methods
         void draw(int termHeight, int termWidth);
         void drawChar(int coordX, int coordY, string content, string color);
-        void label();
-        void move();
         string getChar(int coordX, int coordY);
+};
 
-        void lineDataDraw();
-        void barDataDraw();
-        vector<string> lineChartPattern(vector<float> steps);
+
+class Linechart : public Chart {
+
+    // various line characters
+    vector<string> chars = { "╭", "╮", "╰", "╯", "│", "─" };
+    vector<string> charsBold = { "╔", "╗", "╚", "╝", "║", "═" };
+    vector<string> charsArea = { "▗", "▖", "▙", "▟" , "▐" , "▄" };
+    vector<string> sharpChars = { "┌", "┐", "└", "┘", "│", "─" };
+    
+    bool area;
+    
+    public:
+        void label();
+        void dataDraw();
+        vector<string> pattern(vector<float> steps);
+};
+
+
+class Barchart : public Chart {
+    vector<string> chars = { "▁", "▂" "▃", "▄", "▅", "▆", "▇", "█" };
+    
+    public:
+        void label();
+        void dataDraw();
 };
